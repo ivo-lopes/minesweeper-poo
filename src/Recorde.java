69201477c3afb5200182c166d3bcfb6a6b1c735d
Lais Oliveira <107074086+LaisOli22@users.noreply.github.com>
2024-02-20 10:41:13 -0300

    Update Recorde.java

--- src/Recorde.java
+++ src/Recorde.java
@@ -1,14 +1,22 @@
 package minesweeper.tabuleiro;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Recorde {
 	private static final int MAX_PONTUACOES = 10;
     private List<Pontuacao> pontuacoes;
 
     public Recorde() {
-        pontuacoes = new ArrayList<>();
+        this.pontuacoes = new ArrayList<>();
+        carregarPontuacoes();
     }
 
     public void adicionarPontuacao(String nomeJogador, long tempo, boolean venceu) {
@@ -17,11 +25,51 @@ public class Recorde {
         if (pontuacoes.size() > MAX_PONTUACOES) {
             pontuacoes.remove(MAX_PONTUACOES);
         }
+        salvarPontuacoes();
+        
+    }
+    
+    private void carregarPontuacoes() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("pontuacoes.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    String nomeJogador = parts[0];
+                    long tempo = Long.parseLong(parts[1]);
+                    boolean venceu = Boolean.parseBoolean(parts[2]);
+                    pontuacoes.add(new Pontuacao(nomeJogador, tempo, venceu));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void salvarPontuacoes() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("pontuacoes.txt"))) {
+            for (Pontuacao pontuacao : pontuacoes) {
+                writer.write(String.format("%s,%d,%b%n", pontuacao.getNomeJogador(), pontuacao.getTempo(), pontuacao.isVenceu()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Pontuacao> getPontuacoes() {
         return pontuacoes;
     }
+    
+    public List<Pontuacao> getTop10Pontuacoes() {
+    	List<Pontuacao> pontuacoes = getPontuacoes();
+    	List<Pontuacao> pontuacoesVencedoras = pontuacoes.stream()
+    	.filter(Pontuacao::isVenceu).sorted(Comparator.comparing(Pontuacao::getTempo)).collect(Collectors.toList());
+
+    	int tamanhoLista = Math.min(pontuacoesVencedoras.size(), MAX_PONTUACOES);
+        return pontuacoesVencedoras.subList(0, tamanhoLista);
+
+    }
+    
 }
 
 class Pontuacao {
@@ -46,5 +94,6 @@ class Pontuacao {
     public boolean isVenceu() {
         return venceu;
     }
+
 
 }
